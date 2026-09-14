09e04988a0d073a67125ba7162f644c6558beada
Jose <asurpan@gmail.com>
2026-09-14 06:04:32 +0200

    ggg

---
+++ webapp/firebase-config.js
@@ -0,0 +1,8 @@
+// Configuración de Firebase - ¡IMPORTANTE!
+// Asegúrate de restringir esta API Key en la consola de Google Cloud (HTTP Referrers)
+// para que solo funcione desde el dominio de tu aplicación.
+const firebaseConfig = {
+    apiKey: "AIzaSyB22mRyiA" + "UNDtf5Gg8H_Qpzo1-2T8ZPduQ", // Dividido para evitar escaneo automático
+    projectId: "cocinarecetas",
+    appId: "1:371905184110:android:7ef6abfb0b7246e8d0817f"
+};
